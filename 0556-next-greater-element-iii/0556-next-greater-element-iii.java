81d60a8e48c270fc48d650609915339e407e0656
Gargi Singh <gargisinghmarch@gmail.com>
2026-03-18 00:57:30 +0530

    Time: 0 ms (100%), Space: 41.9 MB (84.22%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        char[] arr = String.valueOf(n).toCharArray();
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+        if (i < 0) return -1;
+        int j = arr.length - 1;
+        while (arr[j] <= arr[i]) {
+            j--;}
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        reverse(arr, i + 1, arr.length - 1);
+        long result = Long.parseLong(new String(arr));
+        return (result > Integer.MAX_VALUE) ? -1 : (int) result;
+    }
+    private void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
+    }
+}
