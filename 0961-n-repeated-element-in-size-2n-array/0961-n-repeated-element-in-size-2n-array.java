4861b85845162facae600df175f416d69102cd4b
Gargi Singh <gargisinghmarch@gmail.com>
2026-05-02 15:15:15 +0530

    Time: 0 ms (100%), Space: 47.4 MB (69.64%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
